aa28a150882a22b141bdeb2ec95e8470150d5f79
Shyam <shyam.achuthan@above-inc.com>
2018-08-23 14:32:02 +0530

    Adding json responses

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+
+function jsonError(res,data = null,message = 'Error',status=200) {
+    let jsondata =  {
+        meta:{
+            success:false,
+            message:message,
+        },
+        data:data
+    };
+
+  res.status(status).json(jsondata);
+}
+
+function jsonSuccess(res,data = null,message='Success',status=200) {
+    let jsondata =  {
+        meta:{
+            success:true,
+            message:message,
+        },
+        data:data
+    };
+
+  res.status(status).json(jsondata);
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/api', function(req, res, next) {
+  jsonSuccess(res,[],"Good")
 });
 
 module.exports = router;
